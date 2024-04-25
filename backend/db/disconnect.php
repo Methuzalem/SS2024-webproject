f12d46bf8ce8d7abf58e1934d417f7d105849d9e
Marvin <m.loehlein@gmx.at>
2024-04-25 23:17:14 +0200

    DB Test added at servicehandler

---
+++ backend/db/disconnect.php
@@ -0,0 +1,7 @@
+<?php
+
+function closeDBConnection($conn) {
+    $conn->close();
+}
+
+?>
--- backend/servicehandler.php
+++ backend/servicehandler.php
@@ -1,7 +1,21 @@
 <?php
-include "./db/connection.php"
+include "./db/connection.php";
+
+// Testabfrage
+$sql = "SELECT * FROM appointments";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["Appo_ID"] . " - Name: " . $row["title"] . "- date: " . $row["date"] . "- date of exp: " . $row["expire"] . "- adress: ". $row["adress"] . "<br>";
+    }
+} else {
+    echo "0 Ergebnisse";
+}
 
 
 
 
+
+include "./db/disconnect.php"
 ?>
