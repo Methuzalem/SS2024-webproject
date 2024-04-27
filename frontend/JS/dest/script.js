dbaf19d6767c1b4b9679dc5a699a99e8cb3fbe5d
Christoph Gartner <gartner-christoph@gmx.at>
2024-04-27 22:27:34 +0200

    worked on html layout and style of header

--- frontend/JS/dest/script.js
+++ frontend/JS/dest/script.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("test");
+var modal = document.getElementById("newAppointmentModal");
+var btn = document.getElementById("newAppointment");
+var cls = document.getElementById("close");
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function (e) {
+    console.log("Modal Open");
+    modal === null || modal === void 0 ? void 0 : modal.setAttribute("style", "display:block");
+});
+cls === null || cls === void 0 ? void 0 : cls.addEventListener("click", function (e) {
+    console.log("Modal Close");
+    modal === null || modal === void 0 ? void 0 : modal.setAttribute("style", "display:none");
+});
