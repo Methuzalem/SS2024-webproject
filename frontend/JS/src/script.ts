ed81a526009391b8d75cfa3f1dad4d998fa11548
Christoph Gartner <gartner-christoph@gmx.at>
2024-04-28 02:43:52 +0200

    worked on style of index.html

--- frontend/JS/src/script.ts
+++ frontend/JS/src/script.ts
@@ -1,13 +1,1 @@
-const modal = document.getElementById("newAppointmentModal") as HTMLElement;
-const btn   = document.getElementById("newAppointment")      as HTMLInputElement;
-const cls   = document.getElementById("close")               as HTMLInputElement;
 
-btn?.addEventListener("click", e => {
-    console.log("Modal Open");
-    modal?.setAttribute("style", "display:block");
-})
-
-cls?.addEventListener("click", e => {
-    console.log("Modal Close");
-    modal?.setAttribute("style", "display:none");
-})
