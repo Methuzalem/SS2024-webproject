fae7158b987d49c367f11a31e796bf8ed7601d0a
Christoph Gartner <gartner-christoph@gmx.at>
2024-04-29 11:11:13 +0200

    added modal functioniality to list

--- frontend/JS/dest/script.js
+++ frontend/JS/dest/script.js
@@ -1,1 +1,5 @@
 "use strict";
+var m = document.getElementById("div1");
+var n = document.getElementById("appoinmentModal");
+m === null || m === void 0 ? void 0 : m.addEventListener("shown.bs.modal", function () {
+});
--- frontend/JS/src/script.ts
+++ frontend/JS/src/script.ts
@@ -1,1 +1,5 @@
+const m = document.getElementById("div1") as HTMLInputElement;
+const n = document.getElementById("appoinmentModal") as HTMLElement;
 
+m?.addEventListener("shown.bs.modal", () => {
+});
