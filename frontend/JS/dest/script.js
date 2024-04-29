3e08ec91b87b7532a420b10aba2aab92a0234f92
Christoph Gartner <gartner-christoph@gmx.at>
2024-04-29 15:41:40 +0200

    finished change to ui and style

--- frontend/JS/dest/script.js
+++ frontend/JS/dest/script.js
@@ -1,5 +1,32 @@
 "use strict";
-var m = document.getElementById("div1");
-var n = document.getElementById("appoinmentModal");
-m === null || m === void 0 ? void 0 : m.addEventListener("shown.bs.modal", function () {
+var newClose = document.getElementById("newClose");
+var newSave = document.getElementById("newSave");
+var listTitle = document.getElementById("listTitle");
+var listDuration = document.getElementById("listDuration");
+var listDate = document.getElementById("listDate");
+var newTitle = document.getElementById("newTitle");
+var newDuration = document.getElementById("newDuration");
+var newDate = document.getElementById("newDate");
+var list = document.getElementById("list");
+var listElement = "<li class=\"list-group-item border-dark\" data-bs-toggle=\"modal\" data-bs-target=\"#appointmentModal\"></li>";
+function clearNew() {
+    newTitle.value = "";
+    newDuration.value = "";
+    newDate.value = "";
+}
+function appendListElement() {
+    list.innerHTML = listElement;
+}
+function appendAppointmentData() {
+}
+function createNewAppointment() {
+    appendListElement();
+    appendAppointmentData();
+    clearNew();
+}
+newClose === null || newClose === void 0 ? void 0 : newClose.addEventListener("click", function () {
+    clearNew();
+});
+newSave === null || newSave === void 0 ? void 0 : newSave.addEventListener("click", function () {
+    createNewAppointment();
 });
--- frontend/JS/src/script.ts
+++ frontend/JS/src/script.ts
@@ -1,5 +1,45 @@
-const m = document.getElementById("div1") as HTMLInputElement;
-const n = document.getElementById("appoinmentModal") as HTMLElement;
+const newClose = document.getElementById("newClose") as HTMLInputElement;
+const newSave  = document.getElementById("newSave")  as HTMLInputElement;
 
-m?.addEventListener("shown.bs.modal", () => {
-});
+const listTitle    = document.getElementById("listTitle")    as HTMLInputElement;
+const listDuration = document.getElementById("listDuration") as HTMLInputElement;
+const listDate     = document.getElementById("listDate")     as HTMLInputElement;
+
+const newTitle    = document.getElementById("newTitle")    as HTMLInputElement;
+const newDuration = document.getElementById("newDuration") as HTMLInputElement;
+const newDate     = document.getElementById("newDate")     as HTMLInputElement;
+
+const list        = document.getElementById("list") as HTMLDataListElement;
+const listElement = "<li class=\"list-group-item border-dark\" data-bs-toggle=\"modal\" data-bs-target=\"#appointmentModal\"></li>";
+
+function clearNew()
+{
+    newTitle.value = "";
+    newDuration.value = "";
+    newDate.value = "";
+}
+
+function appendListElement()
+{
+    list.innerHTML = listElement;
+}
+
+function appendAppointmentData()
+{
+    
+}
+
+function createNewAppointment() 
+{
+    appendListElement();
+    appendAppointmentData();
+    clearNew();
+}
+
+newClose?.addEventListener("click", () => {
+    clearNew();
+})
+
+newSave?.addEventListener("click", () => {
+    createNewAppointment();
+})
