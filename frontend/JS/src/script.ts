51f66fef1681e082e8fca37038164d93fb716bc6
Marvin <m.loehlein@gmx.at>
2024-05-02 01:27:08 +0200

    Quality of Life implemented for Appo Creation. Reload fixed db data delete

--- frontend/JS/src/script.ts
+++ frontend/JS/src/script.ts
@@ -101,18 +101,53 @@ function refreshPage(): void {
     window.location.reload();
 }
 
+function canSave(): boolean {
+    return newTitle.value.trim() !== "" &&
+           newDate.value.trim() !== "" &&
+           newTime.value.trim() !== "" &&
+           newDuration.value.trim() !== "" &&
+           newLocation.value.trim() !== "";
+}
+
 newSave?.addEventListener('click', () => {
-    sendData('../backend/logic/appocreation.php')
-        .then(() => {
-            clearNew();
-            refreshPage(); 
-        })
-        .catch(error => {
-            console.error('Error sending data:', error);
-        });
+    if (canSave()) {
+        sendData('../backend/logic/appocreation.php')
+            .then(() => {
+                clearNew();
+                refreshPage(); 
+            })
+            .catch(error => {
+                console.error('Error sending data:', error);
+            });
+    } else {
+        alert('Please fill out all fields before saving!')
+    }
 })
 
 document.addEventListener('DOMContentLoaded', function (){ 
-    fetch('../backend/servicehandler.php');
-    fetchData("../backend/JSON/Appointments.json");
+    fetch('../backend/servicehandler.php')
+        .then(() => {
+            fetchData("../backend/JSON/Appointments.json");
+        });
+
+    //fix clicking outside modal box (delete values)
+    var newAppointmentModal = document.getElementById('newAppointmentModal');
+    var modalIsClosedByUser = true;
+
+    newSave.addEventListener('click', function () {
+        modalIsClosedByUser = false;
+    });
+
+    newClose.addEventListener('click', function () {
+        modalIsClosedByUser = false;
+    });
+
+    if(newAppointmentModal){
+        newAppointmentModal.addEventListener('hidden.bs.modal', function () {
+            if (modalIsClosedByUser) {
+                clearNew();
+            }
+            modalIsClosedByUser = true; 
+        });
+    }
 });
