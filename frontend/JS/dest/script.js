f049285b0ac84fd4ff26cf51ab4dc3156a4276f9
Christoph Gartner <gartner-christoph@gmx.at>
2024-05-03 02:01:01 +0200

    commentated code

--- frontend/JS/dest/script.js
+++ frontend/JS/dest/script.js
@@ -1,15 +1,18 @@
 "use strict";
+//Declaration of buttons used in modal windows
 var newClose = document.getElementById("newClose");
 var newSave = document.getElementById("newSave");
 var addDate = document.getElementById("addDate");
 var appointmentClose = document.getElementById("appointmentClose");
 var appointmentSave = document.getElementById("appointmentSave");
+//declaration of input elements used in creating a new appointment
 var newTitle = document.getElementById("newTitle");
 var newDate = document.getElementById("newDate");
 var newExpire = document.getElementById("newExpire");
 var newTime = document.getElementById("newTime");
 var newDuration = document.getElementById("newDuration");
 var newLocation = document.getElementById("newLocation");
+//declaration of input elements used in viewing the detailed version of an appointment
 var appointmentID = "";
 var appointmentTitle = document.getElementById("appointmentTitle");
 var appointmentDuration = document.getElementById("appointmentDuration");
@@ -19,6 +22,7 @@ var appointmentName = document.getElementById("appointmentName");
 var appointmentDate = document.getElementById("appointmentDate");
 var appointmentTime = document.getElementById("appointmentTime");
 var appointmentComment = document.getElementById("appointmentComment");
+//declaration of input elements used in the expired version of appointments
 var expiredTitle = document.getElementById("expiredTitle");
 var expiredDuration = document.getElementById("expiredDuration");
 var expiredLocation = document.getElementById("expiredLocation");
@@ -26,12 +30,14 @@ var expiredExpire = document.getElementById("expiredExpire");
 var list = document.getElementById("list");
 var data;
 var dates = [];
+//removes all elements from the appointment list
 function clearList() {
     var elements = list.querySelectorAll("li");
     for (var i = 0; i < elements.length; i++) {
         list.removeChild(elements[i]);
     }
 }
+//clears all input elements for creating a new appointment
 function clearNew() {
     newTitle.value = "";
     newDate.value = "";
@@ -40,21 +46,26 @@ function clearNew() {
     newTime.value = "";
     newLocation.value = "";
 }
+//clears all elements for viewing the detailed version
 function clearAppointment() {
     appointmentName.value = "";
     appointmentTime.value = "";
     appointmentComment.value = "";
+    //clears all generated options for the selector element
     var options = appointmentDate.querySelectorAll("option");
     for (var i = 1; i < options.length; i++) {
         appointmentDate.removeChild(options[i]);
     }
 }
+//generates appointment list elements from json data
 function appendAppointments(data) {
     clearList();
     for (var i = 0; i < data.length; i++) {
+        //creates a list element with let to clear it once for scope reaches end
         var li = document.createElement("li");
         li.setAttribute("id", i.toString());
         for (var j = 0; j < 2; j++) {
+            //every list elemented seperated into two divs with an input tag each
             var div = document.createElement("div");
             var input = document.createElement("input");
             input.setAttribute("class", "form-control-plaintext");
@@ -62,17 +73,19 @@ function appendAppointments(data) {
             input.readOnly = true;
             switch (j) {
                 case 0:
+                    //first input element holds the title of the appointment
                     div.setAttribute("class", "appointmentTitle");
                     input.value = data[i].title;
                     break;
                 case 1:
+                    //second one holds the duration of the appointment
                     input.value = "Duration: " + data[i].duration + " hours";
             }
             div.appendChild(input);
             li.appendChild(div);
         }
-        var temp = new Date();
-        console.log(temp.toISOString().split('T')[0]);
+        //each list element is set to open a detail modal window
+        //when appointment is not expired the version in which can be voted is called
         li.setAttribute("data-bs-toggle", "modal");
         li.setAttribute("data-bs-target", "#appointmentModal");
         if (data[i].expired == 0) {
@@ -85,6 +98,7 @@ function appendAppointments(data) {
         list.appendChild(li);
     }
 }
+//function fetches json data from appointments.json and saves it to global data variable
 function fetchData(url) {
     fetch(url)
         .then(function (response) {
@@ -101,6 +115,7 @@ function fetchData(url) {
         console.error("Fehler beim Laden der JSON-Datei:", error);
     });
 }
+//function sends post method to service handler.php to save new appointments
 function sendDataAppo(url) {
     var datesString = dates.join(",");
     return fetch(url, {
@@ -118,6 +133,7 @@ function sendDataAppo(url) {
         console.error('Error sending data:', error);
     });
 }
+//function sends post method to service handler.php to save votes for appointments
 function sendDataVote(url) {
     return fetch(url, {
         method: 'POST',
@@ -140,6 +156,7 @@ newClose === null || newClose === void 0 ? void 0 : newClose.addEventListener("c
 function refreshPage() {
     window.location.reload();
 }
+//checks if all input elements are filled when creating a new appointment
 function canSave() {
     return newTitle.value.trim() !== "" &&
         newExpire.value.trim() !== "" &&
@@ -147,16 +164,20 @@ function canSave() {
         newDuration.value.trim() !== "" &&
         newLocation.value.trim() !== "";
 }
+//checks if all input elements are filled when voting for appointment
 function canSaveAppointment() {
     return appointmentName.value.trim() !== "" &&
         appointmentDate.value.trim() !== "" &&
         appointmentTime.value.trim() !== "" &&
         appointmentComment.value.trim() !== "";
 }
+//generates date options from json data
 function generateAppointmentOptions(fetchedData, id) {
+    //for each date entry check if appointment keys match and is an option
     for (var i = 0; i < fetchedData.length; i++) {
         if (fetchedData[i].isOption == 1) {
             if (fetchedData[i].appointment == data[id].Appo_ID) {
+                //create option with inner text of date and append to select element
                 var option = document.createElement("option");
                 option.setAttribute("value", i.toString());
                 option.innerHTML = fetchedData[i].date;
@@ -169,7 +190,7 @@ function loadExpireModal(id) {
     expiredTitle.innerHTML = data[id].title;
     expiredDuration.value = data[id].duration;
     expiredLocation.value = data[id].location;
-    expiredExpire.value = data[id].date;
+    expiredExpire.value = data[id].expireDate;
 }
 function loadVoteModal(id) {
     appointmentTitle.innerHTML = data[id].title;
@@ -177,6 +198,8 @@ function loadVoteModal(id) {
     appointmentLocation.value = data[id].location;
     appointmentExpire.value = data[id].expireDate;
     appointmentID = data[id].Appo_ID;
+    //appointment detail modal holds select element for which options need to be dynamically generated
+    //passes date.json data to generateAppointmentOptions
     fetch("../backend/JSON/Date.json")
         .then(function (response) {
         if (!response.ok) {
@@ -194,6 +217,7 @@ function loadVoteModal(id) {
 appointmentClose === null || appointmentClose === void 0 ? void 0 : appointmentClose.addEventListener('click', function () {
     clearAppointment();
 });
+//sends appointment vote data to database when save button is clicked
 appointmentSave === null || appointmentSave === void 0 ? void 0 : appointmentSave.addEventListener('click', function () {
     sendDataVote('../backend/logic/votecreation.php')
         .then(function () {
@@ -204,6 +228,7 @@ appointmentSave === null || appointmentSave === void 0 ? void 0 : appointmentSav
     });
     clearAppointment();
 });
+//sends new appointment data to database when save button is clicked
 newSave === null || newSave === void 0 ? void 0 : newSave.addEventListener('click', function () {
     if (canSave()) {
         sendDataAppo('../backend/logic/appocreation.php')
@@ -219,6 +244,8 @@ newSave === null || newSave === void 0 ? void 0 : newSave.addEventListener('clic
         alert('Please fill out all fields before saving!');
     }
 });
+//saves selected dates to array and resets input element
+//that way infinite date options for single appointment can be selected
 addDate === null || addDate === void 0 ? void 0 : addDate.addEventListener("click", function () {
     if (newDate.value != "") {
         dates.push(newDate.value);
@@ -226,14 +253,17 @@ addDate === null || addDate === void 0 ? void 0 : addDate.addEventListener("clic
     console.log(dates);
     newDate.value = "";
 });
+//function gets id of clicked list element when list is clicked
 list === null || list === void 0 ? void 0 : list.addEventListener("click", function (event) {
     var target = event.target;
     var li = target;
+    //counts to the highest clicked list element
     while (li && li.nodeName !== 'LI') {
         li = li.parentNode;
     }
     if (!li)
         return;
+    //gets id of clicked list element
     var id = li.getAttribute('id');
     if (id) {
         if (data[parseInt(id)].expired == 1)
@@ -242,6 +272,7 @@ list === null || list === void 0 ? void 0 : list.addEventListener("click", funct
             loadVoteModal(parseInt(id));
     }
 });
+//loads list from appointment.json when page is loaded
 document.addEventListener('DOMContentLoaded', function () {
     fetch('../backend/servicehandler.php')
         .then(function () {
@@ -258,6 +289,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     if (newAppointmentModal) {
         newAppointmentModal.addEventListener('hidden.bs.modal', function () {
+            clearAppointment();
             if (modalIsClosedByUser) {
                 clearNew();
             }
--- frontend/JS/src/script.ts
+++ frontend/JS/src/script.ts
@@ -1,10 +1,11 @@
+//Declaration of buttons used in modal windows
 const newClose = document.getElementById("newClose") as HTMLInputElement;
 const newSave  = document.getElementById("newSave")  as HTMLInputElement;
 const addDate  = document.getElementById("addDate")  as HTMLInputElement;
-
 const appointmentClose = document.getElementById("appointmentClose") as HTMLInputElement;
 const appointmentSave  = document.getElementById("appointmentSave")  as HTMLInputElement;
 
+//declaration of input elements used in creating a new appointment
 const newTitle    = document.getElementById("newTitle")    as HTMLInputElement;
 const newDate     = document.getElementById("newDate")     as HTMLInputElement;
 const newExpire   = document.getElementById("newExpire")   as HTMLInputElement;
@@ -12,6 +13,7 @@ const newTime     = document.getElementById("newTime")     as HTMLInputElement;
 const newDuration = document.getElementById("newDuration") as HTMLInputElement;
 const newLocation = document.getElementById("newLocation") as HTMLInputElement;
 
+//declaration of input elements used in viewing the detailed version of an appointment
 let appointmentID = "";
 const appointmentTitle    = document.getElementById("appointmentTitle")    as HTMLInputElement;
 const appointmentDuration = document.getElementById("appointmentDuration") as HTMLInputElement;
@@ -22,6 +24,7 @@ const appointmentDate     = document.getElementById("appointmentDate")     as HT
 const appointmentTime     = document.getElementById("appointmentTime")     as HTMLInputElement;
 const appointmentComment  = document.getElementById("appointmentComment")  as HTMLInputElement;
 
+//declaration of input elements used in the expired version of appointments
 const expiredTitle    = document.getElementById("expiredTitle") as HTMLInputElement;
 const expiredDuration = document.getElementById("expiredDuration") as HTMLInputElement;
 const expiredLocation = document.getElementById("expiredLocation") as HTMLInputElement;
@@ -32,6 +35,7 @@ const list = document.getElementById("list") as HTMLDataListElement;
 var data : any[];
 var dates : string[] = [];
 
+//removes all elements from the appointment list
 function clearList()
 {
     let elements = list.querySelectorAll("li");
@@ -41,6 +45,7 @@ function clearList()
     }
 }
 
+//clears all input elements for creating a new appointment
 function clearNew()
 {
     newTitle.value = "";
@@ -51,11 +56,13 @@ function clearNew()
     newLocation.value = "";
 }
 
+//clears all elements for viewing the detailed version
 function clearAppointment()
 {
     appointmentName.value = "";
     appointmentTime.value = "";
     appointmentComment.value = "";
+    //clears all generated options for the selector element
     const options = appointmentDate.querySelectorAll("option");
     for(let i = 1; i < options.length; i++)
     {
@@ -63,18 +70,20 @@ function clearAppointment()
     }
 }
 
+//generates appointment list elements from json data
 function appendAppointments(data: any)
 {
     clearList();
     for(let i = 0; i < data.length; i++)
     {
+        //creates a list element with let to clear it once for scope reaches end
         let li = document.createElement("li");
         li.setAttribute("id", i.toString());
 
         for(let j = 0; j < 2; j++)
         {
+            //every list elemented seperated into two divs with an input tag each
             let div = document.createElement("div");
-
             let input = document.createElement("input");
             input.setAttribute("class", "form-control-plaintext");
             input.setAttribute("type", "text");
@@ -83,20 +92,21 @@ function appendAppointments(data: any)
             switch(j)
             {
                 case 0:
+                    //first input element holds the title of the appointment
                     div.setAttribute("class", "appointmentTitle");
                     input.value = data[i].title;
                     break;
                 case 1:
+                    //second one holds the duration of the appointment
                     input.value = "Duration: " + data[i].duration + " hours";
             }
 
             div.appendChild(input);
             li.appendChild(div);
         }
-        
-        let temp = new Date();
-        console.log(temp.toISOString().split('T')[0]);
 
+        //each list element is set to open a detail modal window
+        //when appointment is not expired the version in which can be voted is called
         li.setAttribute("data-bs-toggle", "modal");
         li.setAttribute("data-bs-target", "#appointmentModal");
         if(data[i].expired == 0)
@@ -113,6 +123,7 @@ function appendAppointments(data: any)
     }
 }
 
+//function fetches json data from appointments.json and saves it to global data variable
 function fetchData(url: string) : any
 {
     fetch(url)
@@ -131,6 +142,7 @@ function fetchData(url: string) : any
     });
 }
 
+//function sends post method to service handler.php to save new appointments
 function sendDataAppo(url: string)
 {
     var datesString = dates.join(",");
@@ -150,6 +162,7 @@ function sendDataAppo(url: string)
     });
 }
 
+//function sends post method to service handler.php to save votes for appointments
 function sendDataVote(url: string)
 {
     return fetch(url, {
@@ -176,6 +189,7 @@ function refreshPage(): void {
     window.location.reload();
 }
 
+//checks if all input elements are filled when creating a new appointment
 function canSave(): boolean {
     return newTitle.value.trim() !== "" &&
            newExpire.value.trim() !== "" &&
@@ -184,6 +198,7 @@ function canSave(): boolean {
            newLocation.value.trim() !== "";
 }
 
+//checks if all input elements are filled when voting for appointment
 function canSaveAppointment(): boolean {
     return appointmentName.value.trim() !== "" &&
            appointmentDate.value.trim() !== "" &&
@@ -191,14 +206,17 @@ function canSaveAppointment(): boolean {
            appointmentComment.value.trim() !== "";
 }
 
+//generates date options from json data
 function generateAppointmentOptions(fetchedData: any[], id: number)
 {
+    //for each date entry check if appointment keys match and is an option
     for(let i = 0; i < fetchedData.length; i++)
     {
         if(fetchedData[i].isOption == 1)
         {
             if(fetchedData[i].appointment == data[id].Appo_ID)
             {
+                //create option with inner text of date and append to select element
                 let option = document.createElement("option");
                 option.setAttribute("value", i.toString());
                 option.innerHTML = fetchedData[i].date;
@@ -213,7 +231,7 @@ function loadExpireModal(id: number)
     expiredTitle.innerHTML = data[id].title;
     expiredDuration.value  = data[id].duration;
     expiredLocation.value  = data[id].location;
-    expiredExpire.value    = data[id].date;
+    expiredExpire.value    = data[id].expireDate;
 }
 
 function loadVoteModal(id: number)
@@ -225,6 +243,8 @@ function loadVoteModal(id: number)
 
     appointmentID = data[id].Appo_ID;
 
+    //appointment detail modal holds select element for which options need to be dynamically generated
+    //passes date.json data to generateAppointmentOptions
     fetch("../backend/JSON/Date.json")
     .then(response => {
         if (!response.ok) {
@@ -244,6 +264,7 @@ appointmentClose?.addEventListener('click', () => {
     clearAppointment();
 })
 
+//sends appointment vote data to database when save button is clicked
 appointmentSave?.addEventListener('click', () => {
     sendDataVote('../backend/logic/votecreation.php')
         .then(() => {
@@ -255,6 +276,7 @@ appointmentSave?.addEventListener('click', () => {
     clearAppointment();
 })
 
+//sends new appointment data to database when save button is clicked
 newSave?.addEventListener('click', () => {
     if (canSave()) {
         sendDataAppo('../backend/logic/appocreation.php')
@@ -270,6 +292,8 @@ newSave?.addEventListener('click', () => {
     }
 })
 
+//saves selected dates to array and resets input element
+//that way infinite date options for single appointment can be selected
 addDate?.addEventListener("click", function(){
     if(newDate.value != "")
     {
@@ -279,14 +303,17 @@ addDate?.addEventListener("click", function(){
     newDate.value = "";
 })
 
+//function gets id of clicked list element when list is clicked
 list?.addEventListener("click", function(event){
     const target = event.target as HTMLElement;
     let li = target;
+    //counts to the highest clicked list element
     while(li && li.nodeName !== 'LI')
     {
         li = li.parentNode as HTMLElement;
     }
     if(!li) return;
+    //gets id of clicked list element
     const id = li.getAttribute('id');
     if(id)
     {
@@ -297,6 +324,7 @@ list?.addEventListener("click", function(event){
     }
 })
 
+//loads list from appointment.json when page is loaded
 document.addEventListener('DOMContentLoaded', function (){ 
     fetch('../backend/servicehandler.php')
         .then(() => {
@@ -317,10 +345,11 @@ document.addEventListener('DOMContentLoaded', function (){
 
     if(newAppointmentModal){
         newAppointmentModal.addEventListener('hidden.bs.modal', function () {
-            if (modalIsClosedByUser) {
+            clearAppointment();
+            if(modalIsClosedByUser) {
                 clearNew();
             }
-            modalIsClosedByUser = true; 
+            modalIsClosedByUser = true;
         });
     }
 });
